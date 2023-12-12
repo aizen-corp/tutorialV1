04230b0551598a3fcf5597b5591efe627d959659
hiteshnettam24 <60822550+hiteshnettam24@users.noreply.github.com>
2023-12-12 11:19:01 +0530

    simplified training model files

--- foresight_licensed/trip_fare/trip_fare_prediction_model_1/trip_fare_1_dl_model_train.ml
+++ foresight_licensed/trip_fare/trip_fare_prediction_model_1/trip_fare_1_dl_model_train.ml
@@ -4,24 +4,18 @@ using_foresight_options
 	{
         "query_type": "training",
         "model": { "name": "trip_fare_1_dl_model" },
-        "dl_config": {
-            "input_features": [
-                     { "name": "pickup_zipcode", "type": "category" },
-                     { "name": "dropoff_zipcode", "type": "category" },
-                     { "name": "passenger_count", "type": "number",
-                       "preprocessing": {"normalization":"zscore", "missing_value_strategy":"fill_with_mean"} }
+        "dl_config": 
+        {
+            "input_features": 
+            [
+                { "name": "pickup_zipcode", "type": "category" },
+                { "name": "dropoff_zipcode", "type": "category" },
+                { "name": "passenger_count", "type": "number" }
             ],
-            "output_features": [
-              { "name": "fare_amount", "type": "number",
-                "num_fc_layers":4, "fc_size":128, "norm":"batch"}
-            ],
-            "trainer" : {
-                "batch_size": 1024,
-                "epochs": 150,
-                "early_stop": 8,
-                "learning_rate": 0.001,
-                "optimizer": {"type": "adam"}
-            }
+            "output_features": 
+            [
+                { "name": "fare_amount", "type": "number"}
+            ]
         }
-    }
+    }    
 )
