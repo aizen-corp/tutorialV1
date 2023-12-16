dc2bb64b04b0198be9c379e9c44dafe31eb16425
hiteshnettam24 <60822550+hiteshnettam24@users.noreply.github.com>
2023-12-16 08:15:45 +0530

    added trainer options to simplified DL training files

--- foresight_licensed/trip_fare/trip_fare_prediction_model_2/trip_fare_2_dl_model_train.ml
+++ foresight_licensed/trip_fare/trip_fare_prediction_model_2/trip_fare_2_dl_model_train.ml
@@ -2,24 +2,26 @@ select * from trip_fare_2_train_dataset
 using_foresight_options
 (
 	{
-    "query_type": "training",
-    "model": { "name": "trip_fare_2_dl_model" },
-    "dl_config":
-    {
-        "input_features": 
-        [
-          { "name": "pickup_zipcode", "type": "category" },
-          { "name": "dropoff_zipcode", "type": "category" },
-          { "name": "passenger_count", "type": "numerical"},
-          { "name": "hourly_segment", "type": "category" },
-          { "name": "is_holiday_or_weekend", "type": "numerical" },
-          { "name": "pickup_geo_area", "type": "category" },
-          { "name": "dropoff_geo_area", "type": "category" }
-        ],
-        "output_features": 
-        [
-          { "name": "fare_amount", "type": "numerical"}
-        ]
+        "query_type": "training",
+        "model": { "name": "trip_fare_2_dl_model" },
+        "dl_config": {
+            "input_features": [
+                     { "name": "pickup_zipcode", "type": "category" },
+                     { "name": "dropoff_zipcode", "type": "category" },
+                     { "name": "passenger_count", "type": "numerical"},
+                     { "name": "hourly_segment", "type": "category" },
+                     { "name": "is_holiday_or_weekend", "type": "numerical" },
+                     { "name": "pickup_geo_area", "type": "category" },
+                     { "name": "dropoff_geo_area", "type": "category" }
+            ],
+            "output_features": [
+              { "name": "fare_amount", "type": "numerical"}
+            ],
+            "trainer" : {
+                "batch_size": 2048,
+                "epochs": 100,
+                "early_stop": 5
+            }
+        }
     }
-  }
 )
