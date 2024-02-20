c7f98628ee030db6ddce535077991fca5669093d
venkat1m <venkatm@aizencorp.com>
2024-02-20 10:34:30 -0800

    Add report format to explore queries

--- foresight_licensed/trip_fare/trip_fare_prediction_model_2/trip_fare_2_train_dataset_data_quality.ml
+++ foresight_licensed/trip_fare/trip_fare_prediction_model_2/trip_fare_2_train_dataset_data_quality.ml
@@ -5,6 +5,7 @@ using_foresight_options
         "query_type": "data-quality",
 	"datetime_column": "pickup_datetime",
 	"target_column": "fare_amount",
-	"limit": -1
+	"limit": -1,
+        "report_format": "pdf"
     }
 )
