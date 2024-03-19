0a55301608eed4b4204ee70b244da8fcf9e56196
Anuradha Hegde <anuradhah@elevo.ai>
2024-03-18 17:39:53 -0700

    Fixed issues seen when running with foresightd free edition

--- foresight_free/trip_fare/trip_fare_prediction_model_2/trip_fare_2_model_train.ml
+++ foresight_free/trip_fare/trip_fare_prediction_model_2/trip_fare_2_model_train.ml
@@ -11,7 +11,7 @@ using_foresight_options
                 "type": "regression",
                 "target": "fare_amount",
                 "numeric_features": ["passenger_count","is_holiday_or_weekend"],
-                "categorical_features": ["pickup_zipcode","dropoff_zipcode","hourly_segment","pickup_geo_area","dropoff_geo_area"],
+                "categorical_features": ["hour_of_day","pickup_zipcode","dropoff_zipcode","hourly_segment","pickup_geo_area","dropoff_geo_area","calendar_day"],
                 "date_features": ["pickup_datetime"]
             }   
         }
